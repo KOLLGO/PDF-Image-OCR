acdb623c45d8c9b51affa84b670cd635afbb88c3
KOLLGO <134609826+KOLLGO@users.noreply.github.com>
2025-05-14 19:39:41 +0200

    var.py closes #2
    closes #2

---
+++ var.py
@@ -0,0 +1,8 @@
+# Path for tesseract.exe (default path already put in)
+tesseract_path = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+# Path of the Document
+input_path = ""
+# Output Folder (example path)
+output_path = ".\\out\\"
+# Language ("eng" for english)
+lang = "deu+eng"
